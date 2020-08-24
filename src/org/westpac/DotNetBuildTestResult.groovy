93ac4535f5c8d9a27ab1d6f10e2a33a0c7775e45
Narendra Tumkur <ntumkur@EN110393.local>
2020-08-24 09:55:55 +0100

    Adds DotNetBuildTestResult class for .net projects

---
+++ src/org/westpac/DotNetBuildTestResult.groovy
@@ -0,0 +1,23 @@
+package org.westpac
+
+public class DotNetBuildTestResult implements BuildTestResult{
+    def jenkinsSteps
+    DotNetBuildTestResult(jenkinsSteps) {
+        this.jenkinsSteps = jenkinsSteps
+    }
+
+    @Override
+    void build() {
+        this.jenkinsSteps.echo "Building .net project..."
+    }
+
+    @Override
+    void test() {
+        this.jenkinsSteps.echo "Testing .net project..."
+    }
+
+    @Override
+    void result() {
+        this.jenkinsSteps.echo "Storing artifacts..."
+    }
+}
