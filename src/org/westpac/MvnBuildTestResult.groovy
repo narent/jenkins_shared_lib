33fbd634d72b5b033ace56b211e860b84e225291
Narendra Tumkur <ntumkur@EN110393.local>
2020-08-24 09:44:03 +0100

    Adds dummy content to build steps

--- src/org/westpac/MvnBuildTestResult.groovy
+++ src/org/westpac/MvnBuildTestResult.groovy
@@ -3,16 +3,16 @@ package org.westpac
 public class MvnBuildTestResult implements BuildTestResult{
     @Override
     void build() {
-
+        println "Building maven project..."
     }
 
     @Override
     void test() {
-
+        println "Testing maven project..."
     }
 
     @Override
     void result() {
-
+        println "Storing artifacts..."
     }
 }
